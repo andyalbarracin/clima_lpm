826371a267ebaefe3196d0848152305bd9398eed
andy.albarra <albarracin.andres@gmail.com>
2022-09-14 16:48:48 -0300

    agregando objeto 1

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const listaCiudades = []
 let cantidadCiudades = 3
 
+
+sesion = 1
+while( sesion <= 1) {
+  const ciudad ={}
+  ciudad.index = 1
+  ciudad.nombre = prompt("En que ciudad te encuentras?:")
+  ciudad.grados = parseInt(prompt("Cuantos grados hace en este momento?(Ingresa solo números):"))
+  
+  
+  sesion++;
+}
+let entrada = prompt("Ingresa hasta 3 nombres de ciudades:")
+document.write(`<h1>Bienvenidos! Vamos a ver que tal el clima en </br>${ciudad.nombre}</h1>`)
+document.write(`<h2>Ciudad Elegida: ${ciudad.index} de ${ciudad.name}</h2>`)
+
+
+/*
+const listaCiudades = []
+let cantidadCiudades = 3
 do {
   let entrada = prompt("Ingresa hasta 3 nombres de ciudades:")
   listaCiudades.push(entrada.toLocaleUpperCase())
@@ -16,15 +35,14 @@ const recorreCiudades = (arr) => {
 
 recorreCiudades(listaCiudades);
 
-let elegir = prompt(`Elija la ciudad:
+let elegir = parseInt(prompt(`Elija la ciudad:
  1. ${listaCiudades[0]}
  2. ${listaCiudades[1]}
  3. ${listaCiudades[2]}
- `);
+ `));
 
  
-document.write(`<h2>Ciudad Elegida: ${elegir}</h2>`);
-document.write(`<h2>Ciudad Elegida: ${listaCiudades[elegir-1]}</h2>`)
-
+document.write(`<h2>Ciudad Elegida: ${elegir} - ${listaCiudades[elegir-1]}</h2>`)
+*/
 
 
