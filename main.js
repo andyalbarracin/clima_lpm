c67f027e196b7edd9ea4678193e1985c0aa49b31
andy.albarra <albarracin.andres@gmail.com>
2022-09-20 22:51:04 -0300

    arrays

--- main.js
+++ main.js
@@ -1,54 +1,60 @@
 
-// Creamos el objeto "Ciudad"
-const ciudad ={}
-ciudad.index = 1
-ciudad.valor = true
-/*Estos datos serán tomados desde la ubicación del usuario mediante una API*/
-ciudad.nombre = prompt("En que ciudad te encuentras?:")
-ciudad.grados = parseInt(prompt("Cuantos grados hace en este momento? (Ingresa solo números):"))
+// Declaración
+class City {
+  constructor(id, nombre, tempe) {
+    this.id = id;
+    this.nombre = nombre;
+    this.tempe = tempe;
+  }
 
-//para controlar los datos ingresados
-console.log(ciudad)
-
-// Funcion para mostrar el mensaje
-let gradosAhora = ciudad.grados
-console.log(gradosAhora)
-const mostrarMensaje = (gradosAhora) => {
-  if (gradosAhora <= 11) {
-    document.write(`<h1>Bienvenidos! Vamos a ver que tal el clima en </br><strong>${ciudad.nombre.toLocaleUpperCase()}</strong></h1>`)
-    document.write(`<h2>Parece que hacen unos ${ciudad.grados}º de temperatura.</h2>`)
-    document.write(`<h3>Mejor abrigate.</h3`);
-  }else if (gradosAhora >= 12 && gradosAhora < 19){
-    document.write(`<h1>Bienvenidos! Vamos a ver que tal el clima en </br><strong>${ciudad.nombre.toLocaleUpperCase()}</strong></h1>`)
-    document.write(`<h2>Parece que hacen unos ${ciudad.grados}º de temperatura.</h2>`)
-    document.write(`<h3>LLeva un buzo por las dudas</h3>`);
-  }else if (gradosAhora >= 20 && gradosAhora < 29) {
-    document.write(`<h1>Bienvenidos! Vamos a ver que tal el clima en </br><strong>${ciudad.nombre.toLocaleUpperCase()}</strong></h1>`)
-    document.write(`<h2>Parece que hacen unos ${ciudad.grados}º de temperatura.</h2>`)
-    document.write(`<h3>Tranqui, disfruta que el día esta bueno.</h3>`);
-  }else if (gradosAhora >= 30 && gradosAhora <= 55) {
-    document.write(`<h1>Bienvenidos! Vamos a ver que tal el clima en </br><strong>${ciudad.nombre.toLocaleUpperCase()}</strong></h1>`)
-    document.write(`<h2>Parece que hacen unos ${ciudad.grados}º de temperatura.</h2>`)
-    document.write(`<h3>Boludo! Quedate en tu casa o te quemas!</h3>`);
-  }else {
-    document.write(`<h3>Ingresá una temperatura válida</h3>`);
+  toString() {
+    return `${this.id}. ${this.nombre}, ${this.tempe} º`;
   }
 }
 
-//Llamamos a la funcion mostrarMensaje con el argumento gradosAhora
-mostrarMensaje(gradosAhora)
+const arrayCity = [];
+arrayCity.push(new City(1, "Mendoza", 14));
+arrayCity.push(new City(2, "Ushuaia", 6));
+arrayCity.push(new City(3, "Neuquen", 11));
+arrayCity.push(new City(4, "Jujuy", 18));
+arrayCity.push(new City(5, "Salta", 22));
 
 
-/*
-- Quiero sumar iconos dependiendo el resultado del clima
-- Quiero sumar mas paramentros, como humedad y sensacion térmica
-  para tener mucha mas variedad de mensajes.
-- Seguramente implemente un coeficiente con estos valores mencionados, 
-  de ese resultado se derivaran los mensajes
-- Muchas más validaciones para los ingresos del usuario.
-  */
+const mensaje = (listaCiudades) => {
+  let auxMensaje = [];
+  auxMensaje.push("Elija las ciudades que quiere tener información: ");
+  for (let i = 0; i < listaCiudades.length; i++) {
+    auxMensaje.push(listaCiudades[i].toString());
+  }
+  auxMensaje.push("Para finalizar su selección ingrese 0.");
+  return auxMensaje.join("\n");
+};
 
+let elegirCiudades;
+let buscarCiudades;
+const choice = [];
+let tempe = 0;
 
-console.log(ciudad)
-console.log(ciudad.grados)
-console.log(gradosAhora)
+// Ejecución
+elegirCiudades = parseInt(prompt(mensaje(arrayCity)));
+
+while (
+  elegirCiudades > 0 &&
+  elegirCiudades <= arrayCity.length &&
+  !isNaN(elegirCiudades)
+) {
+  buscarCiudades = arrayCity.find((el) => el.id === elegirCiudades);
+  choice.push(buscarCiudades);
+  elegirCiudades = parseInt(prompt(mensaje(arrayCity)));
+}
+
+if (choice.length > 0) {
+  choice.forEach((el) => (tempe += el.tempe));
+  alert(
+    `Usted seleccionó:\n${choice.join(
+      "\n"
+    )}\nSon lugares agradables?`
+  );
+} else {
+  alert("¡No has seleccionado nada!");
+}
